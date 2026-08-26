a8836a010a05245e0f61cede7cbbe7e71e69e5b1
Kong <vicky.kong@lausd.net>
2026-08-26 03:54:26 +0000

    Lab #5

---
+++ vkong5.py
@@ -0,0 +1,24 @@
+#Vicky Kong
+#Lab 5
+
+nums=[12,8,44,68,2,32,74,100,56,2068]
+
+print(nums[0])
+print(nums[1])
+print(nums[2])
+print(nums[3])
+print(nums[4])
+print(nums[5])
+print(nums[6])
+print(nums[7])
+print(nums[8])
+print(nums[9])
+
+#loop version
+for i in nums:
+    print(i)
+
+food=["avacado","spinach","strawberries","bak choy","pineapple"]
+
+for i in food:
+    print(i)
