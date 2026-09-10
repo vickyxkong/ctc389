8f994face62de67bc98a7c6d8e7fa077e2a0b11d
Kong <vicky.kong@lausd.net>
2026-09-10 01:05:19 +0000

    Midterm Question 2

---
+++ vkongM2.py
@@ -0,0 +1,15 @@
+#Vicky Kong
+#Midterm Question 2
+
+def areaofrect(base,height):
+    area=base*height
+    return area
+
+b=int(input("Enter the base of the rectangle:"))
+h=int(input("Enter the height of the rectangle:"))
+
+rectangle=areaofrect(b,h)
+
+print("The area of the rectangle is",rectangle)
+
+
