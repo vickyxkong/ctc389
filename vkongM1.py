6d9c6e1aa4977a062477eb7774743090f8b00f9a
Kong <vicky.kong@lausd.net>
2026-09-10 00:57:56 +0000

    Midterm Question 1

---
+++ vkongM1.py
@@ -0,0 +1,9 @@
+#Vicky Kong
+#Midterm Question 1
+
+name=input("Enter your name:")
+num=int(input("Enter a number:"))
+add=int(input("How much to add to that number?"))
+total=num+add
+
+print(name,"you entered",num,"and added",add,"to get a total of",total)
